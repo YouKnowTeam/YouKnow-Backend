e265ac1bea2638dae9556025cbc17681a9fd7a35
Yan HE <heyanmailbox@gmail.com>
2018-05-29 23:31:53 +0800

    Provide detailed return code for `sign up`

--- database/sign_up.js
+++ database/sign_up.js
@@ -1,6 +1,9 @@
 /*
  * Input: ID, Password
- * Output: true if no duplicate username, false otherwise
+ * Output:
+ * - 0 if success
+ * - -2 if user ID already exists
+ * - -3 if internal error
  */
 module.exports = function(id, pass) {
     var conn = require('./db_conn')();
@@ -12,16 +15,16 @@ module.exports = function(id, pass) {
         if (err) throw err;
         record_num = result.length;
     });
-
-    if (record_num == 0) {
-        var sql2 =
-            "INSERT INTO UserPass (UserID Password) VALUES (?, ?)";
-        conn.query(sql, [id, pass], function (err, result) {
-            if (err) throw err;
-        });
-    }
-
+    
     conn.end();
+    
+    if (record_num > 1) return -3;
+    if (record_num == 1) return -2;
 
-    return (record_num == 0);
+    var sql2 = "INSERT INTO UserPass (UserID Password) VALUES (?, ?)";
+    conn.query(sql, [id, pass], function (err, result) {
+        if (err) throw err;
+    });
+
+    return 0;
 };
