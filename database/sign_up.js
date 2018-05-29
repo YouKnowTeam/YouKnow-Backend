38da94339f4b990296effce54d88058180a23325
Yan HE <heyanmailbox@gmail.com>
2018-05-29 23:52:25 +0800

    Add support for argon2 in `sign in`

--- database/sign_up.js
+++ database/sign_up.js
@@ -6,9 +6,12 @@
  * - -3 if internal error
  */
 module.exports = function(id, pass) {
+    const argon2 = require('argon2');
+
     var conn = require('./db_conn')();
 
     var record_num = 0;
+    var hashed_pass;
 
     var sql1 = "SELECT * FROM UserPass WHERE UserID=?";
     conn.query(sql, [id], function (err, result) {
@@ -20,9 +23,15 @@ module.exports = function(id, pass) {
     
     if (record_num > 1) return -3;
     if (record_num == 1) return -2;
+    
+    argon2.hash('pass').then(hash => {
+        hashed_pass = hash;
+    }).catch(err => {
+        throw err;
+    });
 
     var sql2 = "INSERT INTO UserPass (UserID Password) VALUES (?, ?)";
-    conn.query(sql, [id, pass], function (err, result) {
+    conn.query(sql, [id, hashed_pass], function (err, result) {
         if (err) throw err;
     });
 
