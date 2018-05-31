82cf8fbc51e134088f01ade41cb053c9ec920727
Bo Li <airespringfield@gmail.com>
2018-05-31 18:30:17 +0800

    GetAllSubscriedMsg modified.

--- database/get_all_subscribed_msg.js
+++ database/get_all_subscribed_msg.js
@@ -8,24 +8,44 @@
  */
 var bcrypt = require('bcrypt');
 
-module.exports = function(userid, msgid, num, callback) {
-    
+module.exports = function (userid, msgid, num, callback) {
+
     var conn = require('./db_conn')();
 
-    var record, match_res;
+    var sql;
 
-    var sql1 = "SELECT * FROM Msg WHERE SrcID in (SELECT SrcID FROM UserSrc WHERE UserID=? ) AND MsgID<=?  ORDER BY MsgID DESC limit ?";
-    
+    if (parseInt(msgid) < 0) {
+        sql = "SELECT MsgID, SrcID, Brief, Timestamp FROM Msg WHERE SrcID in (SELECT SrcID FROM UserSrc WHERE UserID=? )  ORDER BY MsgID DESC limit ?"; 
 
-    conn.query(sql1, [userid, parseInt(msgid), parseInt(num)], function (err, result) {
-        var code = 0;
-        if (err) {
-            console.log(err.message);
-            code = -3;
-        }
-        conn.end();
-        
-        callback(code, result);
-    });
-    
+        conn.query(sql, [userid, parseInt(num)], function (err, result) {
+            var code = 0;
+            if (err) {
+                console.log(err.message);
+                code = -3;
+            }
+            conn.end();
+
+            callback(code, result);
+        });
+
+
+    }
+
+    else {
+        sql = "SELECT MsgID, SrcID, Brief, Timestamp FROM Msg WHERE SrcID in (SELECT SrcID FROM UserSrc WHERE UserID=? ) AND MsgID<=?  ORDER BY MsgID DESC limit ?";
+
+
+        conn.query(sql, [userid, parseInt(msgid), parseInt(num)], function (err, result) {
+            var code = 0;
+            if (err) {
+                console.log(err.message);
+                code = -3;
+            }
+            conn.end();
+
+            callback(code, result);
+        });
+
+    }
+
 };
