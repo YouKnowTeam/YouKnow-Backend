172b0a9070ff1c8f9d81e9b93f3aa4f9074e3fbb
JengfengYang <1500012170@pku.edu.cn>
2018-06-01 00:51:34 +0800

    feat: add_msgsrc

--- database/add_msgsrc.js
+++ database/add_msgsrc.js
@@ -4,22 +4,16 @@
  * - 0: if success
  * - -3: if internal error
  */
-module.exports = function(userid, srcid) {
+module.exports = function(userid, srcid, callback) {
     var conn = require('./db_conn')();
-
     var return_code = 0;
-
     var sql = "INSERT INTO UserSrc (UserID, SrcID) VALUES (?, ?)";
     conn.query(sql, [userid, srcid], function (err, result) {
-        /*
         if (err) {
             console.log(err);
             return_code = -3;
         }
-        */
+        conn.end();
+        callback(return_code) 
     });
-
-    conn.end();
-
-    return return_code;
 };
