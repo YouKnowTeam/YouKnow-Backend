43c485353e094317b0bc7115b843b3a4943c1260
JengfengYang <1500012170@pku.edu.cn>
2018-05-31 23:43:53 +0800

    fix(#12): get_msgsrc

--- database/get_msgsrcs.js
+++ database/get_msgsrcs.js
@@ -11,35 +11,39 @@ module.exports = function(userid, callback) {
 
     var sql1 = "SELECT * FROM MsgSrc";
     conn.query(sql1, function (err, result) {
-        
-        if (err) console.log(err);
-        all_src = result;
-        
-    });
-
-    var sql2 = "SELECT * FROM UserSrc WHERE UserID=?";
-    conn.query(sql2, [userid], function (err, result) {
-        
-        if (err) console.log(err);
-        user_src = result;
-        
-    });
-
-    console.log("all_src:"+all_src+typeof(all_src))
-
-    all_src.forEach(elem => {
-        var flag = 0;
-        for (i = 0; i < user_src.length; i++) {
-            if (user_src[i].SrcID == elem.SrcID) {
-                flag = 1;
-                break;
-            }
+        var code=0
+        if (err) {
+            code=-3
+            console.log(err);
         }
-
-        elem.flag = flag;
+        console.log("result:"+result+typeof(result))
+        all_src = result;
+        var sql2 = "SELECT * FROM UserSrc WHERE UserID=?";
+        conn.query(sql2, [userid], function (err, result) {
+            if (err) {
+                code=-3
+                console.log(err);
+            }
+            user_src = result;
+            console.log("all_src:"+all_src+typeof(all_src))
+            all_src.forEach(elem => {
+            var flag = 0;
+            for (i = 0; i < user_src.length; i++) {
+                if (user_src[i].SrcID == elem.SrcID) {
+                    flag = 1;
+                    break;
+                }
+            }
+            elem.flag = flag;
+            conn.end();
+            callback(code, all_src)
+        });       
     });
+});
 
-    conn.end();
+    
+
+    
 
     return all_src;
 };
