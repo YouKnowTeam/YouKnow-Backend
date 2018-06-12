055c7654d168681e5b006865bfdfaf0216d29326
Yan HE <heyanmailbox@gmail.com>
2018-06-12 16:25:24 +0800

    Provide an API for retrieving subscribing userIDs

---
+++ database/get_msg_usrs.js
@@ -0,0 +1,27 @@
+/*
+ * Input: SrcID
+ * Output: Array of the userIDs who subscribed to the message sources
+ */
+var bcrypt = require('bcrypt');
+
+module.exports = function (srcid, callback) {
+
+    var conn = require('./db_conn')();
+
+    var sql;
+    
+    sql = "SELECT UserID FROM UserSrc WHERE SrcID=?";
+        
+
+    conn.query(sql, [srcid], function (err, result) {
+        var code = 0;
+        if (err) {
+            console.log(err.message);
+            code = -3;
+        }
+        conn.end();
+
+        callback(code, result);
+    });
+
+};
