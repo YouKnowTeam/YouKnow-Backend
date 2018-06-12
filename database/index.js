a3cab67b9b05cdb0a1990a6fb4c3c4f2d1b6af80
Yan HE <heyanmailbox@gmail.com>
2018-06-12 16:26:34 +0800

    Make get_src_usr available

--- database/index.js
+++ database/index.js
@@ -5,5 +5,6 @@ module.exports = {
     add_msgsrc: require('./add_msgsrc'),
     rm_msgsrc: require('./rm_msgsrc'),
     get_all_subscribed_msg: require('./get_all_subscribed_msg'),
-    get_new_msg: require('./get_new_msg')
+    get_new_msg: require('./get_new_msg'),
+    get_src_usr: require('./get_src_usr')
 };
