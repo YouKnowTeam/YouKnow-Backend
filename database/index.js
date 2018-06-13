62a94fff906feeed0b472cc7d0f5ebe0258a4fd1
JengfengYang <1500012170@pku.edu.cn>
2018-06-13 19:50:29 +0800

    fix(#50): index.js

--- database/index.js
+++ database/index.js
@@ -5,7 +5,6 @@ module.exports = {
     add_msgsrc: require('./add_msgsrc'),
     rm_msgsrc: require('./rm_msgsrc'),
     get_all_subscribed_msg: require('./get_all_subscribed_msg'),
-    get
     get_new_msg: require('./get_new_msg'),
     get_src_usr: require('./get_src_usr'),
     get_msg_detail:require('./get_msg_detail')
